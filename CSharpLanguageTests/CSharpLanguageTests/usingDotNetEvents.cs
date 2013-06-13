8a242106d1e4c7c5fb141c9e18cece506e3e1286
Geoff Groos <geoff_groos@msn.com>
2013-06-13 00:25:34 -0700

    added my stupid usage of events
    - I'm not thinking the way C# wants me to think...
    - I still think i like Guava or Caliburn Micros event bus better

---
+++ CSharpLanguageTests/CSharpLanguageTests/usingDotNetEvents.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CSharpLanguageTests
+{
+    public class usingDotNetEvents
+    {
+        [TestFixture]
+        public class Testing
+        {
+            [Test]
+            public void tryingToRaiseAnEvent()
+            {
+                var bus = new EventBus();
+
+                bus.LightningBolt += subscribingMethod;
+
+                var @event = new MyEvent();
+                bus.publish(@event);
+
+                @event.SubscriberWasCalled.Should().BeTrue();
+            }
+
+            public void subscribingMethod(MyEvent @event)
+            {
+                @event.SubscriberWasCalled = true;
+            }
+        }
+
+        // Special EventArgs class to hold info about Shapes. 
+        public class MyEvent
+        {
+            public bool SubscriberWasCalled { get; set; }
+        }
+
+        // Base class event publisher 
+        public class EventBus
+        {
+            public event Action<MyEvent> LightningBolt;
+
+            public void publish(MyEvent e)
+            {
+                LightningBolt(e);
+            }
+        } 
+    }
+}
